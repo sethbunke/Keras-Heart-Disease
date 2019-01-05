d656592a47fa5b7854e1d206c2fcd9091e80a609
sjbgit <sjb_strat@hotmail.com>
2019-01-04 16:18:23 -0800

    added mult

--- pt.py
+++ pt.py
@@ -7,3 +7,20 @@ features = torch.randn((1,5))
 weights = torch.randn_like(features)
 
 bias = torch.randn((1,1))
+
+
+
+
+#import torch
+import random
+import numpy as np
+
+features = np.random.rand(1,5)
+weights = np.random.rand(1,5)
+bias = np.random.rand(1,1)
+
+result = np.dot(features, weights.T)
+
+print(result)
+
+print(bias + result)
